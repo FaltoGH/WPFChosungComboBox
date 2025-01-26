2ff35e4ff02fbe581f0bc7baae6312224bcf8df3
FaltoGH <sb051380us@gmail.com>
2025-01-26 17:59:22 +0900

    초기 커밋.

---
+++ ChosungComboBox/ComboBox2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+
+namespace ChosungComboBox
+{
+    internal class ComboBox2:ComboBox
+    {
+
+        internal event DependencyPropertyChangedEventHandler PropertyChanged;
+
+        protected override void OnPropertyChanged(DependencyPropertyChangedEventArgs e)
+        {
+            base.OnPropertyChanged(e);
+            PropertyChanged?.Invoke(this, e);
+        }
+
+    }
+}
